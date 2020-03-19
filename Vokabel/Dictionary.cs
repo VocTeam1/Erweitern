cc8a11646325210f33707540f9f34e565d8f0587
franzi1999 <61967851+franzi1999@users.noreply.github.com>
2020-03-19 13:31:54 +0100

    Add files via upload

---
+++ Vokabel/Dictionary.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Vokabel
+{
+    class Dictionary
+    {
+       private Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        public Dictionary()
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(Properties.Resources.Vokabelliste);
+            XmlNodeList xnList = xml.SelectNodes("/root/row");
+            foreach (XmlNode xn in xnList)
+            {
+                string englisch = xn["Fremdsprache"].InnerText;
+                string deutsch = xn["Deutsch"].InnerText;
+                try
+                {
+                    dictionary.Add(englisch, deutsch);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+        public CurrentQuestions GetNext()
+        {
+            Random rand = new Random();
+            CurrentQuestions currentQuestions = new CurrentQuestions();
+            currentQuestions.answers = new string[4];
+            currentQuestions.question = dictionary.ElementAt(rand.Next(0, dictionary.Count)).Key;
+            currentQuestions.correctID = rand.Next(0, 3);
+            dictionary.TryGetValue(currentQuestions.question, out currentQuestions.answers[currentQuestions.correctID]);
+            for (int i = 0; i < currentQuestions.answers.Length; i++)
+            {
+                if (currentQuestions.answers[i] == null)
+                {
+                    currentQuestions.answers[i] = dictionary.ElementAt(rand.Next(0, dictionary.Count)).Value;
+                }
+            }
+            return currentQuestions;
+        }
+        public bool IsCorrectAnswer(string englisch, string german)
+        {
+            string correct;
+            dictionary.TryGetValue(englisch, out correct);
+            if (correct == german)
+            {
+                return true;
+            }
+            return false;
+        }
+        public string CorrectAnswer(string englisch)
+        {
+            string correct;
+            dictionary.TryGetValue(englisch, out correct);
+            return correct;
+        }
+    }
+
+    struct CurrentQuestions
+    {
+        public int correctID;
+        public string question;
+        public string[] answers;
+    }
+}
---
+++ Vokabel/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vokabel
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary dictionary = new Dictionary();
+        CurrentQuestions currentQuestions;
+        Button[] buttons = new Button[4];
+        LinearGradientBrush defaultButtonBrush = new LinearGradientBrush();
+        public MainWindow()
+        {
+            InitializeComponent();
+            fillWindow();
+            defaultButtonBrush = (LinearGradientBrush) btn_Answer_1.Background;
+            buttons[0] = btn_Answer_1;
+            buttons[1] = btn_Answer_2;
+            buttons[2] = btn_Answer_3;
+            buttons[3] = btn_Answer_4;
+        }
+
+        private void Btn_Answer_1_Click(object sender, RoutedEventArgs e)
+        {
+            check(dictionary.IsCorrectAnswer(txbl_Question.Text, ((Button)sender).Content.ToString()), (Button) sender);
+        }
+
+        private void Btn_Answer_4_Click(object sender, RoutedEventArgs e)
+        {
+            check(dictionary.IsCorrectAnswer(txbl_Question.Text, ((Button)sender).Content.ToString()), (Button)sender);
+        }
+
+        private void Btn_Answer_2_Click(object sender, RoutedEventArgs e)
+        {
+            check(dictionary.IsCorrectAnswer(txbl_Question.Text, ((Button)sender).Content.ToString()), (Button)sender);
+        }
+
+        private void Btn_Answer_3_Click(object sender, RoutedEventArgs e)
+        {
+            check(dictionary.IsCorrectAnswer(txbl_Question.Text, ((Button)sender).Content.ToString()), (Button)sender);
+        }
+
+        private void Button_Next_Click(object sender, RoutedEventArgs e)
+        {
+            fillWindow();
+            btn_Answer_1.Click += Btn_Answer_1_Click;
+            btn_Answer_2.Click += Btn_Answer_2_Click;
+            btn_Answer_3.Click += Btn_Answer_3_Click;
+            btn_Answer_4.Click += Btn_Answer_4_Click;
+
+            btn_Answer_1.Background = defaultButtonBrush;
+            btn_Answer_2.Background = defaultButtonBrush;
+            btn_Answer_3.Background = defaultButtonBrush;
+            btn_Answer_4.Background = defaultButtonBrush;
+            lbl_Richtige_Antwort.Content = "";
+            lbl_User_Antwort.Content = "";
+            btn_Weiter.Visibility = Visibility.Hidden;
+
+        }
+        private void fillLabels(Button button)
+        {
+            lbl_Richtige_Antwort.Content = currentQuestions.answers[currentQuestions.correctID];
+            lbl_User_Antwort.Content = button.Content;
+        }
+        private void fillWindow()
+        {
+            currentQuestions = dictionary.GetNext();
+            btn_Answer_1.Content = currentQuestions.answers[0];
+            btn_Answer_2.Content = currentQuestions.answers[1];
+            btn_Answer_3.Content = currentQuestions.answers[2];
+            btn_Answer_4.Content = currentQuestions.answers[3];
+            txbl_Question.Text = currentQuestions.question;
+        }
+        private void check( bool check, Button sender) {
+
+            if (!check)
+            {
+                sender.Background = Brushes.Red;
+            }
+            fillLabels(sender);
+            buttons[currentQuestions.correctID].Background = Brushes.Green;
+            btn_Weiter.Visibility = Visibility.Visible;
+            btn_Answer_1.Click -= Btn_Answer_1_Click;
+            btn_Answer_2.Click -= Btn_Answer_2_Click;
+            btn_Answer_3.Click -= Btn_Answer_3_Click;
+            btn_Answer_4.Click -= Btn_Answer_4_Click;
+
+        }
+
+    }
+}
